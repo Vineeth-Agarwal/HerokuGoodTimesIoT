d524a194dcc8208e3f236740e591edd56270168b
Vineeth Agarwal <vineeth.agarwal06@gmail.com>
2018-12-06 17:50:05 -0600

    get req

--- routes/index.js
+++ routes/index.js
@@ -23,10 +23,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //  for  parsing  application/x-www-form-urlencoded//  GET  method  route
 app.get('/', function (request, response) {
-  response.send('GET  request  to  the  homepage\n')
-   most_recent=0;
-   mean=0;
-   range=0;
+  response.send('GET  request  to  the  homepage\n '+"most_recent is "+most_recent+"mean is "+ mean+" range is "+range)
+  //  most_recent=0;
+  //  mean=0;
+  //  range=0;
 })
 //  POST  method route
 app.post('/', function (request, response) {
@@ -34,10 +34,10 @@ app.post('/', function (request, response) {
   console.log("x is  " + request.body.reactiontime) 
   let x=request.body.reactiontime
   let y=x.split("&")
-  console.log("y is "+y+" array is 0"+ y[0]+" 1 "+y[1]+" 2 "+y[2])
+  console.log("y is "+y+" y0 "+ y[0]+" y1 "+y[1]+" y2 "+y[2])
   // if
   most_recent=parseInt(y[0])
-  mean==parseInt(y[1])
+  mean=parseInt(y[1])
   range=parseInt(y[2])
   console.log("most_recent is "+most_recent+"mean is "+ mean+" range is "+range)
   response.send('POST  request  to  the  homepage\n')
