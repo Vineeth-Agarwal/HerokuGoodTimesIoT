850afd96e969f134f9de3fb31590df69c5a92faf
Vineeth Agarwal <vineeth.agarwal06@gmail.com>
2018-12-06 19:55:41 -0600

    final changes

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ var bodyParser = require('body-parser')
 let app = express();
 let url = require('url')
 let most_recent=0;
+let min=0;
+let max=0;
 let mean=0;
 let range=0;
 let querystring = require('querystring')
@@ -23,10 +25,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //  for  parsing  application/x-www-form-urlencoded//  GET  method  route
 app.get('/', function (request, response) {
-  response.send('GET  request  to  the  homepage\n '+"most_recent is "+most_recent+"mean is "+ mean+" range is "+range)
-  //  most_recent=0;
-  //  mean=0;
-  //  range=0;
+  response.send('GET  request  to  the  homepage\n '+"most_recent is \n"+most_recent+"min is \n"+ min+"max is \n"+ max
+  +"mean is \n"+ mean+" range is \n"+range)
+   most_recent=0;
+   min=0;
+   max=0;
+   mean=0;
+   range=0;
 })
 //  POST  method route
 app.post('/', function (request, response) {
@@ -37,8 +42,10 @@ app.post('/', function (request, response) {
   console.log("y is "+y+" y0 "+ y[0]+" y1 "+y[1]+" y2 "+y[2])
   // if
   most_recent=parseInt(y[0])
-  mean=parseInt(y[1])
-  range=parseInt(y[2])
+  min=parseInt(y[1])
+  max=parseInt(y[2])
+  mean=parseInt(y[3])
+  range=parseInt(y[4])
   console.log("most_recent is "+most_recent+"mean is "+ mean+" range is "+range)
   response.send('POST  request  to  the  homepage\n')
 })
